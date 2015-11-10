5c7f0e77053e74938fa9c626a54602e66714db64
xc145214 <xc145214@gmail.com>
2015-11-10 15:03:35 +0800

    chapter4  ini config

---
+++ chapter4/src/test/java/com/xc/ConfigurationCreateTest.java
@@ -0,0 +1,35 @@
+package com.xc;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.*;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2015/11/10.
+ */
+public class ConfigurationCreateTest {
+
+    @Test
+    public void test() {
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-config.ini");
+
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+
+        //将SecurityManager设置到SecurityUtils 方便全局使用
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        subject.login(token);
+
+        Assert.assertTrue(subject.isAuthenticated());
+
+    }
+}
